9977eb1886af5e67d8d6a5cd582ad5a1398a1310
George <george@vounder.co.uk>
2024-12-24 14:31:10 +0000

    Remove reference to data.csv in repo

--- progress_dashboard.py
+++ progress_dashboard.py
@@ -4,31 +4,32 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import numpy as np
 from openai import OpenAI
+from data import load_data, generate_target_data
 
 # Set the OpenAI API key
 client = OpenAI(api_key=st.secrets['OPENAI_KEY'])
 
 # File to store logged data (need to change to a DB)
-DATA_FILE = "data.csv"
+# DATA_FILE = "data.csv"
 
-# Constants for target weight data
-TARGET_START_DATE = datetime(2024, 12, 9)
-TARGET_END_DATE = TARGET_START_DATE + pd.Timedelta(days=120)
-START_WEIGHT = 101.3
-TARGET_WEIGHT = 91.3
+# # Constants for target weight data
+# TARGET_START_DATE = datetime(2024, 12, 9)
+# TARGET_END_DATE = TARGET_START_DATE + pd.Timedelta(days=120)
+# START_WEIGHT = 101.3
+# TARGET_WEIGHT = 91.3
 
-# Generate target weight data
-def generate_target_data():
-    target_dates = pd.date_range(TARGET_START_DATE, TARGET_END_DATE)
-    target_weights = [START_WEIGHT - (START_WEIGHT - TARGET_WEIGHT) * (i / len(target_dates)) for i in range(len(target_dates))]
-    return pd.DataFrame({"Date": target_dates, "Target Weight": target_weights})
+# # Generate target weight data
+# def generate_target_data():
+#     target_dates = pd.date_range(TARGET_START_DATE, TARGET_END_DATE)
+#     target_weights = [START_WEIGHT - (START_WEIGHT - TARGET_WEIGHT) * (i / len(target_dates)) for i in range(len(target_dates))]
+#     return pd.DataFrame({"Date": target_dates, "Target Weight": target_weights})
 
-# Load or create the data file
-def load_data():
-    try:
-        return pd.read_csv(DATA_FILE, parse_dates=['Date'])
-    except FileNotFoundError:
-        return pd.DataFrame(columns=['Date', 'Weight', 'Calories', 'Deficit', 'Steps', 'Workout'])
+# # Load or create the data file
+# def load_data():
+#     try:
+#         return pd.read_csv(DATA_FILE, parse_dates=['Date'])
+#     except FileNotFoundError:
+#         return pd.DataFrame(columns=['Date', 'Weight', 'Calories', 'Deficit', 'Steps', 'Workout'])
 
 # Load existing data
 logged_data = load_data()
