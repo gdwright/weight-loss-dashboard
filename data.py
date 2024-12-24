0877aea61081caf55325c5d7a1563399c0788b1d
George <george@vounder.co.uk>
2024-12-24 14:18:59 +0000

    Remove csv and put in s3 bucket

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 import numpy as np
+import boto3
 
 
 # File to store logged data (need to change to a DB)
@@ -21,13 +22,28 @@ def generate_target_data():
     return pd.DataFrame({"Date": target_dates, "Target Weight": target_weights})
 
 # Load or create the data file
-def load_data():
-    try:
-        return pd.read_csv(DATA_FILE, parse_dates=['Date'])
-    except FileNotFoundError:
-        return pd.DataFrame(columns=['Date', 'Weight', 'Calories', 'Deficit', 'Steps', 'Workout'])
+# def load_data():
+#     try:
+#         return pd.read_csv(DATA_FILE, parse_dates=['Date'])
+#     except FileNotFoundError:
+#         return pd.DataFrame(columns=['Date', 'Weight', 'Calories', 'Deficit', 'Steps', 'Workout'])
 
-# Save data back to the file
-def save_data(df):
-    df.to_csv(DATA_FILE, index=False)
+# # Save data back to the file
+# def save_data(df):
+#     df.to_csv(DATA_FILE, index=False)
+
+s3 = boto3.client('s3')
+
+def read_csv_from_s3(bucket_name, file_key):
+    obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+    df = pd.read_csv(obj['Body'], parse_dates=['Date'])
+    return df
+
+# Example usage
+def load_data():
+    bucket_name = 'weight-loss-dashboard'
+    file_key = 'data.csv'
+    df = read_csv_from_s3(bucket_name, file_key)
+    return df
+
 
